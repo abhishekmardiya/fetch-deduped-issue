5a7bf741dd481f0826f0459edf61ed6bbb11b8fe
Abhishek Maradiya <mardiyaabhishek@gmail.com>
2025-11-18 17:03:15 +0530

    Update Next.js configuration to disable strict mode and enable fetch logging; remove console log from getPosts function.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: false,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
 export const getPosts = async () => {
-  console.log("rendering getPosts");
-
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
   return data;
